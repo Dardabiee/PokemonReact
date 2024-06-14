41f1c29f6297d4027ccb6e8284b5ecda643190d4
Muhammad Darda Fathammubienaa <muhammaddarda733@gmail.com>
2024-06-14 09:56:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import pokemonItem from "./compenents/PokemonItems/pokemonItem";
+import PokemonList from "./compenents/PokemonList/pokemonList" ; //p besar dan kecil
+
+function App() {
+  
+  return (
+    <>
+      <PokemonList />
+      
+    </>
+  )
+}
+
+export default App
